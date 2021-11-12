95a46bab94d9b15e1dd333ab2adb9ac9bc5a57f2
ramnauthchris25 <90330628+ramnauthchris25@users.noreply.github.com>
2021-11-12 16:00:44 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Main {
+
+public static void main(String[] args)
+
+{
+
+Random rand = new Random();
+
+int[] arr = new int[200000000];
+
+for (int i = 0; i < arr.length; i++) {
+
+arr[i] = rand.nextInt(10) + 1;
+
+}
+
+long start = System.currentTimeMillis();
+
+System.out.println(Summation.sum(arr));
+
+System.out.println("Single: " + (System.currentTimeMillis() - start));
+
+start = System.currentTimeMillis();
+
+System.out.println(Summation.parallelSum(arr));
+
+System.out.println("Parallel: " + (System.currentTimeMillis() - start));
+
+}
+
+}
+
